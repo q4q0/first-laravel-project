9979e61df5e6e9986cfef612682806dd4144deb6
~/alpha <s201830040@kfupm.edu.sa>
2022-05-27 14:56:02 +0300

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('index', [
+            'categories' => $categories
+        ]);
+    }
+}
